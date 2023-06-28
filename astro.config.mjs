1a3dd536aae038184adaaa3652ccfc256a4373da
osgsm <50955473+oshimax@users.noreply.github.com>
2023-06-28 12:16:09 +0900

    Fix site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://osgsm.github.io/',
+  site: 'https://osgsm.github.io',
   base: '/github-pages-test',
 });
